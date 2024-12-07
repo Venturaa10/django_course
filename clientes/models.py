5c148f5218f68978849bea67166386f70bbc63e2
Venturaa10 <joao.victor.ventura25@gmail.com>
2024-12-07 11:19:14 -0300

    Adicionar verificação de regex e atributo "value" na tag "input" do HTML para receber variável do Django.

--- clientes/models.py
+++ clientes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=50)
-    sobrenome = models.CharField(max_length=50, blank=True, null=True)
+    sobrenome = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     cpf = models.CharField(max_length=12)
 
--- clientes/views.py
+++ clientes/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Cliente, Carro
+import re
 
 def clientes(request):
+    ''' 
+    Condições: 
+    - Verifica se cliente já existe de acordo com o cpf.
+    - Verifica se email está de acordo com o padrão fornecido pelo regex.
+
+    Funções:
+    - zip: Combina elementos de múltiplas listas em tuplas, onde cada tupla contém elementos correspondentes das listas fornecidas.
+
+    Retornos:
+    - Se algum campo estiver invalido, renderiza o template novamente e todos os campos já preenchidos pelo usuario, exceto a campo invalido.
+    - Retorna HttpResponse quando todos os campos validos.
+    '''
     if request.method == 'GET':
         return render(request, 'clientes.html')
+    
     elif request.method == 'POST':
         # Metodo POST, pega(.get) e get('Valor do atributo "name" html')
         nome = request.POST.get('nome')
@@ -12,9 +26,17 @@ def clientes(request):
         email = request.POST.get('email')
         cpf = request.POST.get('cpf')
 
+        cliente = Cliente.objects.filter(cpf=cpf) 
+
         carros = request.POST.getlist('carro')
         placas = request.POST.getlist('placa')
         anos = request.POST.getlist('ano')
+
+        if cliente.exists():
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'email': email, 'carros': zip(carros, placas, anos)}) 
+
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'cpf': cpf, 'carros': zip(carros, placas, anos)}) 
 
         cliente = Cliente(
             nome = nome,
@@ -24,9 +46,8 @@ def clientes(request):
         )
 
         cliente.save()
+
         # Converte as tuplas geradas pela função zip em uma lista.
-        # zip -> Combina elementos de múltiplas listas em tuplas, onde cada tupla contém elementos correspondentes
-        # das listas fornecidas.
         for carro, placa, ano in zip(carros, placas, anos):
             car = Carro(carro=carro, placa=placa, ano=ano, cliente=cliente)
             car.save()
